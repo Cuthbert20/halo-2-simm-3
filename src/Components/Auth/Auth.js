b92b45f66c290d0b865c80c9e0b767122ebfbc1c
Cuthbert20 <knowltonspencer@gmail.com>
2019-08-10 16:52:26 -0600

    register back and front end setup finished

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 export default class Auth extends Component {
     state = {
@@ -10,6 +12,17 @@ export default class Auth extends Component {
             [key]: elm.target.value
         })
     }
+    register = () => {
+        const { username, user_password } = this.state
+        axios.post('/auth/register', {username, user_password})
+        .then(res => {
+            console.log(res.data[0])
+            // this.setState({
+            //     username: '',
+            //     user_password: ''
+            // })
+        })
+    }
     render() {
         console.log(this.state)
         const { username, user_password } = this.state
@@ -18,7 +31,7 @@ export default class Auth extends Component {
                 <input placeholder="username" onChange={e => this.handleChange(e, "username")} value={username} type="text"/>
                 <input placeholder='password' onChange={e => this.handleChange(e, "user_password")} value={user_password} type="password"/>
                 <button>Login</button>
-                <button>Register</button>
+                <Link to='/dashboard' ><button onClick={this.register}>Register</button></Link>
             </div>
         )
     }
