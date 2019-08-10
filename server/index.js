3580b605cee23b53249cbded9d230339eb593bcc
Cuthbert20 <knowltonspencer@gmail.com>
2019-08-10 13:33:23 -0600

    routes set up, server set up to listen with massive, connects to db

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env
+const massive = require('massive')
+const session = require('express-session')
+const ctrl = require('./controller.js')
+
+app.use(express.json())
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}))
+
+massive(CONNECTION_STRING).then(db => {
+    
+    app.set('db', db)
+    
+    app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT} is now listening`))
+
+}).catch(err => console.log(err, 'not connecting to db stu'))
+
